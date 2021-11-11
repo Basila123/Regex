7873d9838d05769eaa61683c471e0aa4d409ced7
Basila123 <basilanoushad92@gmail.com>
2021-11-11 05:48:40 +0530

    Password added

---
+++ Phone.java
@@ -0,0 +1,24 @@
+package com.bridgelabzregex;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Phone {
+
+        /**
+         * This method checks if the entered number is valid or not
+         *
+         * @param Number takes in the parameter as String representation of number
+         * @return it returns a boolean value true if the number is valid
+         */
+        public boolean isNumber(String Number) {
+            Pattern pattern = Pattern.compile("^[0-9]{2}+[ ][0-9]{10}$");
+            Matcher matcher = pattern.matcher(Number);
+            return matcher.matches();
+        }
+
+        //driver code
+
+    }
+
+
+
